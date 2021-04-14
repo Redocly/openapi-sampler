6a54a506944327b0b521d407b5db82bcc7877b14
Jakub Rożek <jakub@stoplight.io>
2021-04-14 15:31:51 +0200

    feat: numeric exclusiveMinimum & exclusiveMaximum

--- src/samplers/number.js
+++ src/samplers/number.js
@@ -1,27 +1,20 @@
 export function sampleNumber(schema) {
-  let res;
-  if (schema.maximum && schema.minimum) {
-    res = schema.exclusiveMinimum ? Math.floor(schema.minimum) + 1 : schema.minimum;
-    if ((schema.exclusiveMaximum && res >= schema.maximum) ||
-      ((!schema.exclusiveMaximum && res > schema.maximum))) {
-      res = (schema.maximum + schema.minimum) / 2;
-    }
-    return res;
-  }
-  if (schema.minimum) {
-    if (schema.exclusiveMinimum) {
-      return Math.floor(schema.minimum) + 1;
-    } else {
-      return schema.minimum;
-    }
-  }
-  if (schema.maximum) {
-    if (schema.exclusiveMaximum) {
-      return (schema.maximum > 0) ? 0 : Math.floor(schema.maximum) - 1;
-    } else {
-      return (schema.maximum > 0) ? 0 : schema.maximum;
-    }
+  if ('minimum' in schema) {
+    return schema.minimum;
   }
 
-  return 0;
+  let res = 0;
+  if ('exclusiveMinimum' in schema) {
+    res = schema.exclusiveMinimum + 1;
+
+    if (res === schema.exclusiveMaximum) {
+      res = (res + schema.exclusiveMaximum - 1) / 2;
+    }
+  } else if ('exclusiveMaximum' in schema) {
+    res = schema.exclusiveMaximum - 1;
+  } else if ('maximum' in schema) {
+    res = schema.maximum;
+  }
+
+  return res;
 }
