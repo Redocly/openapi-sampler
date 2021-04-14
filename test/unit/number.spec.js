9fa5401222744eb914eb88be1d283f849419773c
Jakub Rożek <jakub@stoplight.io>
2021-04-14 15:31:51 +0200

    feat: numeric exclusiveMinimum & exclusiveMaximum

--- test/unit/number.spec.js
+++ test/unit/number.spec.js
@@ -17,13 +17,8 @@ describe('sampleNumber', () => {
     expect(res).to.equal(3);
   });
 
-  it('should return minimum +1 for exclusiveMinimum', () => {
-    res = sampleNumber({minimum: 3, exclusiveMinimum: true});
-    expect(res).to.equal(4);
-  });
-
-  it('should return minimum +1 for exclusiveMinimum', () => {
-    res = sampleNumber({minimum: 3, exclusiveMinimum: true});
+  it('should return exclusiveMinimum + 1 if exclusiveMinimum is specified', () => {
+    res = sampleNumber({exclusiveMinimum: 3});
     expect(res).to.equal(4);
   });
 
@@ -32,8 +27,8 @@ describe('sampleNumber', () => {
     expect(res).to.equal(-3);
   });
 
-  it('should return maximum -1 if maximum is negative and exclusiveMaximum', () => {
-    res = sampleNumber({maximum: -3, exclusiveMaximum: true});
+  it('should return exclusiveMaximum - 1 if exclusiveMaximum is specified', () => {
+    res = sampleNumber({exclusiveMaximum: -3});
     expect(res).to.equal(-4);
   });
 
@@ -43,14 +38,17 @@ describe('sampleNumber', () => {
   });
 
   // (2, 3) -> 2.5
-  it('should return middle point if integer is not possible', () => {
-    res = sampleNumber({minimum: 2, maximum: 3, exclusiveMinimum: true, exclusiveMaximum: true});
+  it('should return middle point if boundary integer is not possible', () => {
+    res = sampleNumber({exclusiveMinimum: 2, exclusiveMaximum: 3});
     expect(res).to.equal(2.5);
   });
 
-  // (2, 3] -> 3
+  // [2, 3] -> 2
+  // (8, 13) -> 9
   it('should return closer to minimum possible int', () => {
-    res = sampleNumber({minimum: 2, maximum: 3, exclusiveMinimum: true});
-    expect(res).to.equal(3);
+    res = sampleNumber({minimum: 2, maximum: 3});
+    expect(res).to.equal(2);
+    res = sampleNumber({exclusiveMinimum: 8, exclusiveMaximum: 13});
+    expect(res).to.equal(9);
   });
 });
